c549547370d188bb0ff4b37748419de35a5d63fd
nathan <njyoung94@gmail.com>
2017-02-22 17:14:36 -0500

    spits out string

--- parse_test.py
+++ parse_test.py
@@ -6,4 +6,10 @@ import requests
 page = requests.get('https://tabs.ultimate-guitar.com/v/van_halen/panama_ver3_tab.htm')
 tree = html.fromstring(page.content)
 
-print(tree)
+tab_contents = tree.xpath('//pre[@class="js-tab-content"]/text()')
+
+print(tab_contents)
+
+file = open("panama_dump.txt", "w+")
+
+file.write(str(tab_contents))
